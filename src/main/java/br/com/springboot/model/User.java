f8c8ff141b763fbae09c04894fafe056fca848dd
João Andrade <joaovictorvieira.23@hotmail.com>
2022-05-26 15:59:05 -0500

    feat: chande model user

--- src/main/java/br/com/springboot/model/User.java
+++ src/main/java/br/com/springboot/model/User.java
@@ -1,5 +1,12 @@
 package br.com.springboot.model;
 
+// import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,8 +16,15 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
+    // @Column(name = "nome")
     private String name;
     private String username;
 }
